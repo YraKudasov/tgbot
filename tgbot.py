2f051443261254f2ba998bd689bc2bd8e74a06c6
Yra Kudasov <kudasov5@mail.ru>
2024-04-07 15:04:58 +0300

    add message handler

--- tgbot.py
+++ tgbot.py
@@ -2,3 +2,11 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot('6883397009:AAE_zS_E1I8iTMuwu1AkHKXQMywJV-mZeQ0')
+
+@bot.message_handler(commands=['start'])
+def newChat(message):
+    bot.send_message(message.chat.id,
+                     f'Здравствуйте, с каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
+    
+
+bot.infinity_polling()
