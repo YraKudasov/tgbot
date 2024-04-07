cfa6e8e296e93437e44e3d66b10e71cadd8ce5f5
Yra Kudasov <kudasov5@mail.ru>
2024-04-07 15:35:52 +0300

    add username in new message

--- tgbot.py
+++ tgbot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('6883397009:AAE_zS_E1I8iTMuwu1AkHKXQMywJV-mZeQ0')
 @bot.message_handler(commands=['start'])
 def newChat(message):
     bot.send_message(message.chat.id,
-                     f'Здравствуйте, с каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
+                     f'Здравствуйте, <b>{message.from_user.first_name}</b>.\nС каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
     
 
 bot.infinity_polling()
