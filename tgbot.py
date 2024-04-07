be1ef36a98d0f53734ff0068eeee0c6157f64bbb
Yra Kudasov <kudasov5@mail.ru>
2024-04-07 23:47:08 +0300

    add spheres command

--- tgbot.py
+++ tgbot.py
@@ -9,4 +9,23 @@ def newChat(message):
                      f'Здравствуйте, <b>{message.from_user.first_name}</b>.\nС каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
     
 
+@bot.message_handler(commands=['spheres'])
+def spheresCommand(message):
+    bot.send_message(message.chat.id, f'Сферы, в которых бот может вам помочь:\n\n'
+                                      f'Безопасность\n'
+                                      f'Благоустройство\n'
+                                      f'Газ и топливо\n'
+                                      f'Благоустройство\n'
+                                      f'Государственная собственность\n'
+                                      f'Дороги\n'
+                                      f'ЖКХ\n'
+                                      f'Здравоохранение/Медицина\n'
+                                      f'Коронавирус\n'
+                                      f'Культура\n'
+                                      f'МФЦ/Мои документы\n'
+                                      f'Мобилизация\n'
+                                      f'Мусор/Свалки/ТКО\n'
+                                      f'Образование\n', parse_mode='HTML')
+    
+
 bot.infinity_polling()
