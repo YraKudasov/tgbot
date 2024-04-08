c0359ce5cf011f1aa166a295846cbd7188c56d61
YraKudasov <kudasov5@mail.ru>
2024-04-08 09:31:35 +0300

    add wait message

--- tgbot.py
+++ tgbot.py
@@ -44,6 +44,7 @@ def answerQuestion(message):
     btn3 = types.InlineKeyboardButton('❌ Тему', callback_data='changeTheme')
     btn4 = types.InlineKeyboardButton('❌ Исп. орг.', callback_data='changeDepartment')
     markup.row( btn2, btn3, btn4)
+    bot.send_message(message.chat.id, 'Подождите пожалуйста, ваш запрос обрабатывается...')
     
     
 
