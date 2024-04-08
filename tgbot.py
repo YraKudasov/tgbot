86d0b72f8cbac1131e2db1ad65cdf464675f9670
YraKudasov <kudasov5@mail.ru>
2024-04-08 09:44:56 +0300

    add images

--- tgbot.py
+++ tgbot.py
@@ -39,9 +39,9 @@ def process_new_string(message):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Все ключевые элементы выделены верно', callback_data='sendToDepartment')
     markup.row(btn1)
-    btn2 = types.InlineKeyboardButton(' Гр. тем', callback_data='changeGroups')
-    btn3 = types.InlineKeyboardButton(' Тему', callback_data='changeTheme')
-    btn4 = types.InlineKeyboardButton(' Исп. орг.', callback_data='changeDepartment')
+    btn2 = types.InlineKeyboardButton('❌ Гр. тем', callback_data='changeGroups')
+    btn3 = types.InlineKeyboardButton('❌ Тему', callback_data='changeTheme')
+    btn4 = types.InlineKeyboardButton('❌ Исп. орг.', callback_data='changeDepartment')
     markup.row(btn2, btn3, btn4)
 
 
